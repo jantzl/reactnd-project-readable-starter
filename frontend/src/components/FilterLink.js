9568d791eefc5acaa71c602d59f536679751b961
Lauren OMeara <jantzl@gmail.com>
2017-10-28 16:39:21 -0500

    fix: acive link set to exact

--- frontend/src/components/FilterLink.js
+++ frontend/src/components/FilterLink.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const FilterLink = ({ filter, children }) => (
-  <NavLink
+  <NavLink exact
     to={filter === 'SHOW_ALL' ? '/' : `/${ filter }`}
     activeStyle={{
       textDecoration: 'none',
